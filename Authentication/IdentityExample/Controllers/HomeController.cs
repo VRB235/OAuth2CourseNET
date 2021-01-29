b6d2c50fc5307203bd5ca4157e5d7371d673b0da
Leonardo Azocar <lazocarg.ucab.edu.ve>
2021-01-29 12:58:26 -0400

    Intentando arreglar repo y subiendo Email Verification

--- Authentication/IdentityExample/Controllers/HomeController.cs
+++ Authentication/IdentityExample/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NETCore.MailKit.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,13 +15,16 @@ namespace IdentityExample.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailService _emailService;
 
         public HomeController(
             UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            SignInManager<IdentityUser> signInManager,
+            IEmailService emailService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailService = emailService;
         }
         public IActionResult Index()
         {
@@ -42,7 +46,7 @@ namespace IdentityExample.Controllers
             {
                 var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
 
-                if(signInResult.Succeeded)
+                if (signInResult.Succeeded)
                 {
                     return RedirectToAction("Index");
                 }
@@ -66,17 +70,32 @@ namespace IdentityExample.Controllers
             };
             var result = await _userManager.CreateAsync(user, password);
 
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
-                var signInResult = await _signInManager.PasswordSignInAsync(user, password, false, false);
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
-                if (signInResult.Succeeded)
-                {
-                    return RedirectToAction("Index");
-                }
+                var link = Url.Action(nameof(VerifyEmail), "Home", new { userId = user.Id, code}, Request.Scheme, Request.Host.ToString());
+
+                await _emailService.SendAsync("tets@tes.com","verify email",$"<a href=\"{link}\">Verify Email</a>", true);
+
+                return RedirectToAction("EmailVerification");
             }
 
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> VerifyEmail(string userId, string code)
+        {
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return BadRequest();
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if(result.Succeeded)
+            {
+                return View();
+            }
+
+            return BadRequest();
         }
 
         public IActionResult Register()
@@ -89,6 +108,11 @@ namespace IdentityExample.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index");
+        }
+
+        public IActionResult EmailVerification()
+        {
+            return View();
         }
 
     }
--- Authentication/IdentityExample/Startup.cs
+++ Authentication/IdentityExample/Startup.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NETCore.MailKit.Extensions;
+using NETCore.MailKit.Infrastructure.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,12 @@ namespace IdentityExample
 {
     public class Startup
     {
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +40,7 @@ namespace IdentityExample
                 config.Password.RequireDigit = false;
                 config.Password.RequireNonAlphanumeric = false;
                 config.Password.RequireUppercase = false;
+                config.SignIn.RequireConfirmedEmail = true;
             })
                 .AddEntityFrameworkStores<AppDbContext>() 
                 .AddDefaultTokenProviders(); // Provee un token por default
@@ -39,6 +49,12 @@ namespace IdentityExample
             {
                 config.Cookie.Name = "Identity.Cookie"; // Nombre de la Cookie
                 config.LoginPath = "/Home/Login"; // Ruta de autenticación
+            });
+
+            var mailKitOptions = _config.GetSection("Email").Get<MailKitOptions>();
+
+            services.AddMailKit(config => {
+                config.UseMailKit(mailKitOptions);
             });
 
             services.AddControllersWithViews();
