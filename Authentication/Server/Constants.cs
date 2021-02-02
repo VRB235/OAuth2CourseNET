2b5fee6db77f7b457512b399ed6692f935e3d508
Leonardo Azocar <=>
2021-02-02 10:52:37 -0400

    JWT Bearer

---
+++ Authentication/Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Audiance = "https://localhost:44390/"; // A quien va a estar dirigido el token
+        public const string Issuer = Audiance; // Quien da el token
+        public const string Secret = "not_to_short_secret_otherwise_it_might_throw_an_error"; // Quien da el token
+    }
+}
---
+++ Authentication/Server/Startup.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("OAuth")
+                .AddJwtBearer("OAuth", config =>
+                {
+                    var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+
+                    var key = new SymmetricSecurityKey(secretBytes);
+
+                    // Para obtener el token del URL
+                    config.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if(context.Request.Query.ContainsKey("access_token"))
+                            {
+                                context.Token = context.Request.Query["access_token"];
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+
+                    config.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKey = key,
+                        ValidIssuer = Constants.Issuer,
+                        ValidAudience = Constants.Audiance,
+                    };
+                });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting(); // Mira la ruta a la que se accesa
+
+            app.UseAuthentication(); // Cheque la identidad del usuario
+
+            app.UseAuthorization(); // Chequea la autorización del usuario
+
+            app.UseEndpoints(endpoints => // Utiliza el endpoint al que se accesa
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
