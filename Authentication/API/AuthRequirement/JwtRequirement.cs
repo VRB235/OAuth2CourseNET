cba2213aed42f829e423b2b2641f9010eca0a8a0
Leonardo Azocar <=>
2021-02-03 09:46:15 -0400

    Securing API

---
+++ Authentication/API/AuthRequirement/JwtRequirement.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace API.AuthRequirement
+{
+    public class JwtRequirement : IAuthorizationRequirement 
+    {
+
+    }
+
+    public class JwtRequirementhandler : AuthorizationHandler<JwtRequirement>
+    {
+        private readonly HttpClient _client;
+        private readonly HttpContext _httpContext;
+
+        public JwtRequirementhandler(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _client = httpClientFactory.CreateClient();
+            _httpContext = httpContextAccessor.HttpContext;
+        }
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, JwtRequirement requirement)
+        {
+            if(_httpContext.Request.Headers.TryGetValue("Authorization",out var authHeader))
+            {
+                var accessToken = authHeader.ToString().Split(" ").Last();
+
+                var response = await _client.GetAsync($"https://localhost:44390/oauth/validate?access_token={accessToken}");
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    context.Succeed(requirement);
+                }
+            }
+        }
+    }
+}
