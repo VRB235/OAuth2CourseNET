1d5b00bebf62ad851ad2acc02483cb80ba05df96
Leonardo Azocar <=>
2021-02-04 10:32:04 -0400

    Client Credentials

---
+++ Authentication/IdentityServer/Configuration.cs
@@ -0,0 +1,39 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public static class Configuration
+    {
+        public static IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource> { 
+                new ApiResource
+                {
+                    Name = "ApiOne",
+                    Scopes= { "ApiOne" }
+                } 
+            };
+
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client>
+            {
+                new Client{
+                    ClientId = "client_id",
+                    ClientSecrets = {new Secret("client_secret".ToSha256())},
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "ApiOne" },
+                }
+            };
+
+        public static IEnumerable<ApiScope> GetScopes() =>
+            new List<ApiScope> {
+                new ApiScope("ApiOne"),
+                new ApiScope("ApiTwo"),
+            };
+    }
+}
---
+++ Authentication/IdentityServer/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(Configuration.GetApis())
+                .AddInMemoryClients(Configuration.GetClients())
+                .AddInMemoryApiScopes(Configuration.GetScopes())
+                .AddDeveloperSigningCredential(); // Para crear una firma para el token
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
